6a1e2d39fadffb31d50c56568f4d0c984bf06877
Lupin <mail@lupin.me>
2015-11-30 23:20:14 +0100

    most of it is working

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
-var isDrawerOpened = true;
+var app = angular.module('urss-reader', []);
 
-function drawer(){
-	if (isDrawerOpened == true)
-		closeDrawer();
-	else
-		openDrawer();
-	isDrawerOpened = !isDrawerOpened;
-}
+app.controller("feedsDrawer", ['$scope','FeedService', function ($scope,Feed) {    
+   	$scope.feeds = [{name:"korben", url:"http://korben.info/rss"}, 
+   					{name:"tuxicoman", url:"https://tuxicoman.jesuislibre.net/feed"},
+   					{name:"HackerNews", url:"https://news.ycombinator.com/rss"}
+   					];
+ 	$scope.selectedFeed = "";
+ 	$scope.articles = [];
 
-function openDrawer(){
-	var navigationDrawer = document.getElementById("drawer");
-	var mainContainer = document.getElementById("mainContainer");
-	mainContainer.style.marginLeft = navigationDrawer.style.width;
-	navigationDrawer.className = "drawer shadowed";
-}
 
-function closeDrawer(){
-	var navigationDrawer = document.getElementById("drawer");
-	var mainContainer = document.getElementById("mainContainer");
-	mainContainer.style.marginLeft = 0;
-	navigationDrawer.className = "drawer shadowed closed";
-}
+    $scope.loadFeed=function(e, url){
+    	$scope.selectedFeed = url;        
+        Feed.parseFeed($scope.selectedFeed).then(function(res){
+        	console.log("afterParse :"+res.data.responseData.feed.entries);
+            $scope.articles=res.data.responseData.feed.entries;
+        });
+        console.log("Scope.articles = "+$scope.articles[0]);
+    }
+
+    $scope.loadAllFeeds=function(e){
+    	$scope.articles = [];
+    	angular.forEach($scope.feeds, function(feed) {
+    		Feed.parseFeed(feed.url).then(function(res){
+    			var articles = res.data.responseData.feed.entries;
+    			angular.forEach(articles, function(article) {
+    				$scope.articles.push(article);
+    			})
+    			console.log(res.data.responseData.feed.entries);
+    		});
+    	});
+    }
+
+ 	$scope.loadFeed(null, $scope.feeds[0].url);
+}]);
+
+app.factory('FeedService',['$http',function($http){
+    return {
+        parseFeed : function(url){
+        	console.log("Parsing :"+url);
+            return $http.jsonp('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
+        }
+    }
+}]);
 
