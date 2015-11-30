97fca6931f157019f1836be53c27f655e7d5516d
Lupin <mail@lupin.me>
2015-11-30 23:50:46 +0100

    trying to load data from config file

--- reader.js
+++ reader.js
@@ -1,21 +1,21 @@
+var config_url = "http://lp1.eu/public/uRSS-Reader/config.json"
 var app = angular.module('urss-reader', []);
 
-app.controller("feedsDrawer", ['$scope','FeedService', function ($scope,Feed) {    
-   	$scope.feeds = [{name:"korben", url:"http://korben.info/rss"}, 
-   					{name:"tuxicoman", url:"https://tuxicoman.jesuislibre.net/feed"},
-   					{name:"HackerNews", url:"https://news.ycombinator.com/rss"}
-   					];
+app.controller("feedsDrawer", ['$scope','FeedService','$http', function ($scope,Feed,$http) {
+    $scope.feeds = [];
+    $http.get(config_url).then(function(res){
+        console.log("config="+$scope.config);
+        $scope.config = res.responseData;
+        $scope.feeds = $scope.config.feeds;
+    });
  	$scope.selectedFeed = "";
  	$scope.articles = [];
-
 
     $scope.loadFeed=function(e, url){
     	$scope.selectedFeed = url;        
         Feed.parseFeed($scope.selectedFeed).then(function(res){
-        	console.log("afterParse :"+res.data.responseData.feed.entries);
             $scope.articles=res.data.responseData.feed.entries;
         });
-        console.log("Scope.articles = "+$scope.articles[0]);
     }
 
     $scope.loadAllFeeds=function(e){
@@ -26,18 +26,17 @@ app.controller("feedsDrawer", ['$scope','FeedService', function ($scope,Feed) {
     			angular.forEach(articles, function(article) {
     				$scope.articles.push(article);
     			})
-    			console.log(res.data.responseData.feed.entries);
     		});
     	});
     }
 
- 	$scope.loadFeed(null, $scope.feeds[0].url);
+    if ($scope.feeds.length > 0)
+     	$scope.loadFeed(null, $scope.feeds[0].url);
 }]);
 
 app.factory('FeedService',['$http',function($http){
     return {
         parseFeed : function(url){
-        	console.log("Parsing :"+url);
             return $http.jsonp('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
         }
     }
