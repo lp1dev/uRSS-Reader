ebbbe622d7fbe1d1568da12fc424cf7f3b6f4b6d
Lupin <mail@lupin.me>
2015-12-01 01:08:46 +0100

    added display mode

--- reader.js
+++ reader.js
@@ -1,21 +1,36 @@
-var config_url = "http://lp1.eu/public/uRSS-Reader/config.json"
+var config_url = "http://lp1.eu/public/uSS-Reader/config.json"
 var app = angular.module('urss-reader', []);
 
 app.controller("feedsDrawer", ['$scope','FeedService','$http', function ($scope,Feed,$http) {
-    $scope.feeds = [];
+    $scope.feeds = [{name:"korben", url:"http://korben.info/rss"}];
     $http.get(config_url).then(function(res){
         $scope.config = res.data;
         console.log("config="+$scope.config);
         $scope.feeds = $scope.config.feeds;
-	if ($scope.feeds.length > 0)
-     	    $scope.loadFeed(null, $scope.feeds[0].url);
-    });
- 	$scope.selectedFeed = "";
- 	$scope.articles = [];
+        if ($scope.feeds.length > 0){
+          $scope.loadFeed(null, $scope.feeds[0].url);
+          $scope.pageTitle = $scope.feeds[0].name;
+      }
+  }, function(error){console.log("Config file unreachable")});
+    $scope.articles = [];
+    $scope.displayMode = false;
 
-    $scope.loadFeed=function(e, url){
-    	$scope.selectedFeed = url;        
-        Feed.parseFeed($scope.selectedFeed).then(function(res){
+    $scope.displayArticle=function(e, article){
+        $scope.selectedArticle = article;
+        $scope.pageTitle = $scope.selectedArticle.author + " - " +$scope.selectedArticle.title;
+        $scope.displayMode = true;
+    }
+
+    $scope.hideArticle=function(e){
+        $scope.displayMode = false;
+        $scope.pageTitle = $scope.selectedFeed.name;
+    }
+
+    $scope.loadFeed=function(e, feed){
+        $scope.displayMode = false;
+    	$scope.selectedFeed = feed;        
+        Feed.parseFeed($scope.selectedFeed.url).then(function(res){
+            $scope.pageTitle = $scope.selectedFeed.name;
             $scope.articles=res.data.responseData.feed.entries;
         });
     }
--- res/js/drawer.js
+++ res/js/drawer.js
@@ -11,13 +11,17 @@ function drawer(){
 function openDrawer(){
 	var navigationDrawer = document.getElementById("drawer");
 	var mainContainer = document.getElementById("mainContainer");
+	var contentDisplay = document.getElementById("contentDisplay");
 	mainContainer.style.marginLeft = navigationDrawer.style.width;
+	contentDisplay.style.marginLeft = navigationDrawer.style.width;
 	navigationDrawer.className = "drawer shadowed";
 }
 
 function closeDrawer(){
 	var navigationDrawer = document.getElementById("drawer");
 	var mainContainer = document.getElementById("mainContainer");
+	var contentDisplay = document.getElementById("contentDisplay");
+	contentDisplay.style.marginLeft = 0;
 	mainContainer.style.marginLeft = 0;
 	navigationDrawer.className = "drawer shadowed closed";
 }
